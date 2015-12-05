331eb636ab5016ce874d4aff841c52787213dfd7
Yoshi <hiroluk@gmail.com>
2015-12-04 20:20:13 -0800

    added request to ajax call new routes

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,26 @@ router.get('/gif', function(req, res) {
     // check redis cache first if not there
     // makes call to giphy api anagramized
     // resets cache
-    request("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5",function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body) // Show the HTML for the Google homepage.
-      } else {
-
-      }
-    });
+    var results;
+    setTimeout(function(){
+      console.log(makeGiphyCall());
+    }, 0);
     //res.render('users', {"users": data});
 });
+
+
+
+var makeGiphyCall = function(){
+  var data;
+  request("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaTOxFJmzC&limit=5",function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      return body; // Show the HTML for the Google homepage.
+    } else {
+      return "No results found";
+    }
+  });
+}
+
+
 
 module.exports = router;
